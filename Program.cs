78ad669dc4071e442122a9c36c19d1322cc40e15
Alex Bunting <acbunting.dev@gmail.com>
2023-08-16 16:01:19 -0500

    New class with two methods for creating a new bet and determining winner of a bet.

--- Program.cs
+++ Program.cs
@@ -1,30 +1,16 @@
-﻿using System.Net.WebSockets;
+﻿using BeverageServiceConsoleApp;
+using System.Net.WebSockets;
 
 public class Program
 {
     public static void Main(string[] args)
     {
         Console.WriteLine("Bev Service Buddy\nMake a bet!");
-        Console.WriteLine("Bettor 1: ");
-        var bettorOne = Console.ReadLine();
-        Console.WriteLine("Bettor 2: ");
-        var bettorTwo = Console.ReadLine();
-        Console.WriteLine("How many beverages are at stake?");
-        int numberOfBeverages = int.Parse(Console.ReadLine());
-        Console.WriteLine("{0} and {1} have bet {2} beverages!", bettorOne, bettorTwo, numberOfBeverages);
+        Bet bet = new Bet();
+        bet.CreateNewBet();
+        bet.BetWinner();
+        Console.WriteLine("Bottoms up!");
 
-        Console.WriteLine("Who won?");
-        var winner = Console.ReadLine();
-        if (winner == bettorOne)
-        {
-            Console.WriteLine("{0} won! They won {1} beverages from {2}.", bettorOne, numberOfBeverages, bettorTwo);
-        }
-        else
-        {
-            Console.WriteLine("{0} won! They won {1} beverages from {2}.", bettorTwo, numberOfBeverages, bettorOne);
-        }
-
-        Console.WriteLine("Thanks for playing!\nBottoms Up!");
         Console.Read();
     }
 }
