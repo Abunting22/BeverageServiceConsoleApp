446595e37764e2e70f945d229e98d775854a70c7
Alex Bunting <acbunting.dev@gmail.com>
2023-09-08 15:32:45 -0500

    Added an account creator and login feature connected to UserAccount table in SQL database. Refined some code in SQLDatabaseOperations.

--- Program.cs
+++ Program.cs
@@ -3,17 +3,38 @@ using System.Runtime.CompilerServices;
 
 public class Program
 {
-    private static readonly BetService betService = new();
+    private static readonly BetOperations betService = new();
     private static readonly Bet bet = new();
+    private static readonly Login login = new();
     public static void Main(string[] args)
     {
-        HomePage();
+        Login();
+    }
+
+    public static void Login()
+    {
+        Console.WriteLine("----- Welcome to Bev Buddy -----");
+        Console.WriteLine("1. Login\n2. Create Account\n");
+        var userConsoleInput = int.Parse(Console.ReadLine());
+        switch (userConsoleInput)
+        {
+            case 1:
+                login.AccountLogin();
+                break;
+            case 2:
+                BeverageServiceConsoleApp.Login.CreateNewAccount();
+                login.AccountLogin();
+                break;
+            default:
+                Console.WriteLine("Bottoms Up!");
+                break;
+        }
     }
 
     public static void HomePage()
     {
         Console.WriteLine("----- Welcome to Bev Buddy -----");
-        Console.WriteLine("Main Menu:\n1. Create New Bet\n2. View Bets\n3. Update a Bet\n4. Delete a Bet\n5. Exit");
+        Console.WriteLine("Main Menu:\n1. Create New Bet\n2. View Bets\n3. Search Bet\n4. Update a Bet\n5. Delete a Bet\n6. Exit");
 
         var userConsoleInput = int.Parse(Console.ReadLine()!);
         switch (userConsoleInput)
@@ -27,17 +48,21 @@ public class Program
                 HomePage();
                 break;
             case 3:
-                betService.UpdateBet(bet);
+                betService.SearchBet();
                 HomePage();
                 break;
             case 4:
+                betService.UpdateBet(bet);
+                HomePage();
+                break;
+            case 5:
                 betService.DeleteBet();
                 HomePage();
                 break;
             default:
-                Console.WriteLine("Bottoms Up!");
+                Console.WriteLine("Bottoms Up!\n");
+                Login();
                 break;        
         }
-               
     }
 }
